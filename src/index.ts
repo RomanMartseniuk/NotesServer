cd89703fa6003ed6bb148d7a51309e27df5c5d14
Roman Martseniuk <martseniuk.roma@gmail.com>
2025-08-17 12:13:57 +0300

    complete the server v1.0

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import express, { Request, Response } from "express";
+import multer from "multer";
+import fs from "fs";
+
+const NOTES_DB = "notes.json";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const upload = multer();
+
+const PORT = process.env.PORT || 3000;
+
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
+// Check for the existence of the notes database file
+if (!fs.existsSync(NOTES_DB)) {
+  fs.writeFileSync(NOTES_DB, ""); 
+  console.log("Файл створено:", NOTES_DB);
+}
+
+function readNotesFromFile(): Note[] {
+  try {
+    const data = fs.readFileSync(NOTES_DB, "utf8");
+    return data ? JSON.parse(data) : [];
+  } catch (error) {
+    console.error("Помилка читання файлу:", error);
+    return [];
+  }
+}
+
+app.get("/", (req: Request, res: Response) => {
+  const notes: Note[] = readNotesFromFile();
+  res.json(notes);
+});
+
+app.post("/", upload.none(), (req: Request, res: Response) => {
+  const notes: Note[] = readNotesFromFile();
+  const newNote: Note = {
+    id: Date.now(),
+    title: req.body.title,
+    content: req.body.content
+  };
+  notes.push(newNote);
+
+  fs.writeFileSync(NOTES_DB, JSON.stringify(notes, null, 2));
+  res.status(201).json(newNote);
+
+});
+
+app.delete("/:id", (req: Request, res: Response) => {
+  const notes: Note[] = readNotesFromFile();
+  const noteId = parseInt(req.params.id, 10);
+  const updatedNotes = notes.filter(note => note.id !== noteId);
+  
+  fs.writeFileSync(NOTES_DB, JSON.stringify(updatedNotes, null, 2));
+  res.status(204).send();
+});
+
+app.put("/:id", upload.none(), (req: Request, res: Response) => {
+  const notes: Note[] = readNotesFromFile();
+  const noteId = parseInt(req.params.id, 10);
+  const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+  if (noteIndex === -1) {
+    return res.status(404).send("Note not found");
+  }
+
+  notes[noteIndex] = {
+    ...notes[noteIndex],
+    title: req.body.title,
+    content: req.body.content,
+  };
+
+  fs.writeFileSync(NOTES_DB, JSON.stringify(notes, null, 2));
+  res.json(notes[noteIndex]);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
